3189562b37a2dc4f04e2ff95ffbeb70aea063c90
Eui Lee <lechan1317@gmail.com>
2024-08-22 00:05:15 +0900

    test: api

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,29 @@
-import Image from "next/image";
+"use client";
+
+import SearchBar from "@/components/Input/SearchBar";
 
 export default function Home() {
+  async function getBooks() {
+    const API_URL = `http://www.aladin.co.kr/ttb/api/ItemSearch.aspx?ttbkey=${process.env.NEXT_PUBLIC_ALADDIN_API_KEY}&Query=aladdin&QueryType=Title&MaxResults=10&start=1&SearchTarget=Book&output=xml&Version=20131101`;
+    return fetch(API_URL)
+      .then((res) => res.text()) // 응답을 텍스트로 받음
+      .then((str) => {
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(str, "application/xml");
+        return xmlDoc;
+      });
+  }
+
+  async function handleSearch(query: string) {
+    console.log(query);
+    console.log(process.env.NEXT_PUBLIC_ALADDIN_API_KEY);
+    const books = await getBooks();
+    console.log(books);
+  }
+
   return (
-    <h1>여러 사이트를 돌아다닐 필요없이 모든 책 검색은 책모음에서.</h1>
+    <div className='flex justify-center'>
+      <SearchBar onSearch={handleSearch} />
+    </div>
   );
 }
---
+++ components/Input/SearchBar.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import React, { useState } from "react";
+import { FiSearch } from "react-icons/fi";
+
+interface SearchBarProps {
+  placeholder?: string;
+  onSearch: (query: string) => void;
+}
+
+export default function SearchBar({
+  placeholder = "읽고 싶은 책을 검색해보세요",
+  onSearch,
+}: SearchBarProps) {
+  const [query, setQuery] = useState<string>("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!query.trim()) return;
+    onSearch(query);
+  };
+
+  const handleSearch = () => {
+    if (!query.trim()) return;
+    onSearch(query);
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className='w-[584px] h-[60px] flex items-center px-6 py-4 bg-gray-100 rounded-full'
+    >
+      <input
+        type='text'
+        value={query}
+        onChange={handleChange}
+        placeholder={placeholder}
+        className='w-full h-full bg-transparent outline-none'
+      />
+      <button onClick={handleSearch}>
+        <FiSearch className='w-6 h-6' />
+      </button>
+    </form>
+  );
+}
