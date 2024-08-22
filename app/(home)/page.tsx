b3dd27c8bd3d7fad95d914fd3207acf0ddc7c291
Eui Lee <lechan1317@gmail.com>
2024-08-22 14:29:09 +0900

    feat: header

---
+++ app/(home)/page.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <header className='flex flex-col justify-center items-center gap-8 py-28'>
+      <img src='/LogoIcon.svg' alt='logo Icon' className='w-24 h-24 text-primary' />
+      <div className='flex flex-col gap-5 justify-center items-center'>
+        <div className="text-center text-5xl font-bold text-primary leading-[72px]">
+          여러 사이트를 돌아다닐 필요없이
+          <br />
+          모든 책 검색은 책모음에서.
+        </div>
+        <div className="text-center text-lg font-light text-grey-200 leading-[27px]">
+          도서관, 서점, 전자책 등<br />
+          읽고 싶은 책이 어디에 있는지 한눈에 확인하세요.
+        </div>
+      </div>
+    </header>
+  );
+}
--- components/Navigation/Nav.tsx
+++ components/Navigation/Nav.tsx
@@ -4,7 +4,7 @@ export default function Nav() {
   return (
     <div className='h-[92px] flex px-8 py-4 justify-start items-center gap-6'>
       <Link href='/' className="flex grow shrink basis-0">
-        <img src='/Logo.svg' alt='' />
+        <img src='/Logo.svg' alt='logo' />
       </Link>
       <Link href='/library' className="font-medium">내 서재</Link>
       <Link href='/login' className="font-medium">로그인</Link>
