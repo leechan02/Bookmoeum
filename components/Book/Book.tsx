9801a4d7596634a83cfe63bc6874ccdddb88da65
Eui Lee <lechan1317@gmail.com>
2024-08-22 19:56:53 +0900

    feat: book component

--- components/Book/Book.tsx
+++ components/Book/Book.tsx
@@ -1,8 +1,29 @@
-export default function Book() {
+import { useEffect, useState } from "react";
+
+export default function Book({
+  imageUrl,
+  width,
+}: {
+  imageUrl: string;
+  width: number;
+}): JSX.Element {
+  const [height, setHeight] = useState<number>(0);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = imageUrl;
+    img.onload = () => {
+      const aspectRatio = img.width / img.height;
+      setHeight(width / aspectRatio);
+    };
+  }, [imageUrl, width]);
+
   return (
-    <div>
-      
+    <div
+      className='flex justify-center items-center'
+      style={{ width: `${width}px`, height: `${height}px` }}
+    >
+      <img src={imageUrl} alt='Book Cover' className='w-full h-full' />
     </div>
   );
 }
-
