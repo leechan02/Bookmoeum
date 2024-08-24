471b2c985e0df62828b0c65d16684150e389f4da
Eui Lee <lechan1317@gmail.com>
2024-08-24 17:08:37 +0900

    feat: tabitem upgrade

--- components/Tab/TabItem.tsx
+++ components/Tab/TabItem.tsx
@@ -1,14 +1,33 @@
 interface TabItemProps {
   label: string;
   Icon: React.ElementType;
+  isActive: boolean;
+  onClick: () => void;
 }
 
-export default function TabItem({ label, Icon }: TabItemProps) {
+export default function TabItem({
+  label,
+  Icon,
+  isActive,
+  onClick,
+}: TabItemProps) {
   return (
-    <div className="border-b-2 border-b-primary">
-      <Icon className='w-6 h-6' />
-      <div className="text-primary font-medium text-base">{label}</div>
+    <div
+      className={`border-b-2 cursor-pointer ${
+        isActive ? "border-b-secondary" : "border-b-primary"
+      }`}
+      onClick={onClick}
+    >
+      <Icon
+        className={`w-6 h-6 ${isActive ? "text-secondary" : "text-primary"}`}
+      />
+      <div
+        className={`font-medium text-base ${
+          isActive ? "text-secondary" : "text-primary"
+        }`}
+      >
+        {label}
+      </div>
     </div>
   );
 }
-
