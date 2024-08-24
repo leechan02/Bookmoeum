34489674ac28568ff5b8f4743fc69314d7f4e908
Eui Lee <lechan1317@gmail.com>
2024-08-24 16:38:33 +0900

    feat: fix h-screen

--- app/(normal)/mylibrary/page.tsx
+++ app/(normal)/mylibrary/page.tsx
@@ -2,7 +2,7 @@ import CharacterMsg from "@/components/Message/CharacterMsg";
 
 export default function MylibraryPage() {
   return (
-    <div className="flex justify-center py-60">
+    <div className="flex justify-center h-screen">
       <CharacterMsg msg="앗! 지금 만들고 있는 중이에요!" imageUrl="/FixCat.svg" />
     </div>
   );
