53cca07df4e208980288beb0b03c94aa4a8bb23d
Eui Lee <lechan1317@gmail.com>
2024-08-26 10:17:34 +0900

    feat: search api

--- app/(normal)/search/page.tsx
+++ app/(normal)/search/page.tsx
@@ -2,17 +2,65 @@
 
 import { useSearchParams } from "next/navigation";
 import SearchTabs from "./_components/SearchTabs";
+import { useEffect, useState } from "react";
+
+interface SearchResult {
+  title: string;
+  author: string;
+  cover: string;
+}
 
 export default function SearchPage(): JSX.Element {
   const searchParams = useSearchParams();
   const query = searchParams.get("query") || "검색어를 입력해주세요";
+  const [searchResult, setSearchResult] = useState<SearchResult[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchSearchResults = async () => {
+      if (query === "검색어를 입력해주세요") return;
+
+      setIsLoading(true);
+      try {
+        const response = await fetch(
+          `/api/search?query=${encodeURIComponent(query)}`
+        );
+        if (!response.ok) {
+          throw new Error("API 요청 실패");
+        }
+        const data = await response.json();
+        console.log(data);
+        setSearchResult(data.item || []);
+        console.log(searchResult);
+      } catch (error) {
+        console.error("검색 결과 가져오기 실패:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchSearchResults();
+  }, [query]);
 
   return (
     <>
-    <div className="flex-col justify-center items-center px-28 py-8 gap-8">
-      <div className="font-bold text-3xl text-primary">{query}</div>
-      <SearchTabs />
-    </div>
+      <div className='flex-col justify-center items-center px-28 py-8 gap-8'>
+        <div className='flex-col inline-flex justify-start items-start gap-4'>
+          <div className='font-bold text-3xl text-primary'>{query}</div>
+          <SearchTabs />
+          {isLoading ? (
+            <div>로딩 중...</div>
+          ) : (
+            <div>
+              {searchResult.map((result, index) => (
+                <div key={index}>
+                  <div>{result.title}</div>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
     </>
   );
 }
---
+++ app/api/search/route.tsx
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+
+const TTBkey = process.env.NEXT_PUBLIC_ALADDIN_API_KEY;
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const query = searchParams.get("query");
+
+  if (!query) {
+    return new Response("검색어를 입력해주세요", { status: 400 });
+  }
+
+  const apiUrl = `http://www.aladin.co.kr/ttb/api/ItemSearch.aspx?ttbkey=${TTBkey}&Query=${encodeURIComponent(
+    query
+  )}&QueryType=Keyword&SearchTarget=Book&output=js&Version=20131101
+`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error("API 호출 중 오류 발생:", error);
+    return new Response("API 호출 중 오류가 발생했습니다", { status: 500 });
+  }
+}
