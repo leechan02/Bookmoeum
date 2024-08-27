182c74d2ba2aa39e54e66cdf8ce2071c3a1cf708
Eui Lee <lechan1317@gmail.com>
2024-08-27 15:22:14 +0900

    feat: bookDetail firstSection component

--- app/(normal)/book/[id]/page.tsx
+++ app/(normal)/book/[id]/page.tsx
@@ -7,6 +7,7 @@ import BookStoreIcon from "@/components/Icon/BookStoreIcon";
 import ButtonIcon from "@/components/Icon/ButtonIcon";
 import { useEffect, useState } from "react";
 import { FiBook, FiHeart, FiPlus } from "react-icons/fi";
+import { FirstSection } from "./_components/firstSection";
 
 interface BookDetailParams {
   params: { id: string };
@@ -28,7 +29,7 @@ interface BookData {
   }>;
 }
 
-interface ProcessedBookData extends BookData {
+export interface ProcessedBookData extends BookData {
   processedTitle: string;
   processedAuthor: string;
   translator: string;
@@ -116,56 +117,8 @@ export default function BookDetail({ params }: BookDetailParams) {
   if (!bookData) return <div>No book data found</div>;
 
   return (
-    <div className='flex justify-center items-center py-14'>
-      <div className='flex justify-center items-start gap-40'>
-        <Book imageUrl={bookData.cover} width={240} />
-        <div className='flex-col justify-start items-start gap-20 inline-flex'>
-          <div className='flex-col justify-center items-start gap-6 inline-flex'>
-            <div className='text-3xl font-medium text-primary'>
-              {bookData.processedTitle}
-            </div>
-            <div className='inline-flex justify-start items-center gap-4 px-1'>
-              <div className='inline-flex justify-start items-center gap-1'>
-                <span className='text-sm font-medium text-primary'>
-                  {bookData.processedAuthor}
-                </span>
-                <span className='text-sm font-light text-grey-200'>저</span>
-              </div>
-              {bookData.translator && (
-                <div className='inline-flex justify-start items-center gap-1'>
-                  <span className='text-sm font-medium text-primary'>
-                    {bookData.translator}
-                  </span>
-                  <span className='text-sm font-light text-grey-200'>역</span>
-                </div>
-              )}
-            </div>
-            <div className='inline-flex justify-start items-center gap-1'>
-              <Chip label={bookData.publisher} />
-              <Chip label={bookData.processedCategory} />
-              <Chip label={bookData.pubDate} />
-            </div>
-          </div>
-          <div className="flex-col justify-start items-start gap-8 inline-flex">
-            <div className="flex-col justify-start items-start gap-2 inline-flex">
-              <div className="text-xs font-regular text-grey-200">읽을 수 있는 곳</div>
-              <div className="inline-flex justify-start items-center gap-4">
-                <BookStoreIcon imageUrl="/IconAladdin.svg" width={48} />
-                <BookStoreIcon imageUrl="/IconKyobo.svg" width={48} />
-                <BookStoreIcon imageUrl="/IconYes24.svg" width={48} />
-                <BookStoreIcon imageUrl="/IconYP.svg" width={48} />
-                <BookStoreIcon imageUrl="/IconMille.svg" width={48} />
-                <BookStoreIcon imageUrl="/IconRidi.svg" width={48} />
-                <ButtonIcon Icon={FiPlus} iconSize={48} iconColor="white" bgColor="primary" />
-              </div>
-            </div>
-            <div className="inline-flex justify-start items-center gap-2">
-              <ButtonIcon Icon={FiHeart} iconSize={40} iconColor="white" bgColor="secondary" />
-              <Button Icon={FiBook} label="내 서재에 담기"/>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div>
+      <FirstSection bookData={bookData} />
     </div>
   );
 }
