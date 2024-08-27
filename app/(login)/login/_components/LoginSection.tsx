7bca29a76d5e90bf63a9fc219783dce7a7e8a41b
Eui Lee <lechan1317@gmail.com>
2024-08-27 21:10:51 +0900

    feat: authcontext

--- app/(login)/login/_components/LoginSection.tsx
+++ app/(login)/login/_components/LoginSection.tsx
@@ -1,8 +1,26 @@
+"use client";
+
 import Button from "@/components/Button/Button";
 import Input from "@/components/Input/Input";
+import { auth } from "@/libs/firebase/config";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function LoginSection() {
+  const router = useRouter();
+
+  const handleGoogleLogin = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      await signInWithPopup(auth, provider);
+
+      router.push("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className='inline-flex justify-center items-center w-1/2 bg-white'>
       <div className='flex-col justify-center items-center inline-flex'>
@@ -15,8 +33,14 @@ export default function LoginSection() {
           </div>
           <div className='flex-col justify-center items-center gap-5 inline-flex'>
             <div className='flex-col justify-start items-center gap-4 inline-flex'>
-              <Button icon="/IconGoogle.svg" label="구글로 시작하기" variant="secondary" width={400} />
-              <Button label="다른 옵션 없음" variant="secondary" width={400}/>
+              <Button
+                icon='/IconGoogle.svg'
+                label='구글로 시작하기'
+                variant='secondary'
+                width={400}
+                onClick={handleGoogleLogin}
+              />
+              <Button label='다른 옵션 없음' variant='secondary' width={400} />
             </div>
             <div className='inline-flex justify-center gap-1'>
               <div className='border-b-2 border-secondary w-48 mb-[10px]'></div>
