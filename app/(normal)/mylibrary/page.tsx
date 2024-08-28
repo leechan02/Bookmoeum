0d22f7c7e22968b27525b1610dfc5aebdd53dc14
Eui Lee <lechan1317@gmail.com>
2024-08-28 10:05:48 +0900

    feat: protect mylibrary

--- app/(normal)/mylibrary/page.tsx
+++ app/(normal)/mylibrary/page.tsx
@@ -1,10 +1,17 @@
-import CharacterMsg from "@/components/Message/CharacterMsg";
+"use client";
 
-export default function MylibraryPage() {
+import CharacterMsg from "@/components/Message/CharacterMsg";
+import { withAuth } from "@/contexts/WithAuth";
+
+function MylibraryPage() {
   return (
-    <div className="flex justify-center h-screen">
-      <CharacterMsg msg="앗! 지금 만들고 있는 중이에요!" imageUrl="/FixCat.svg" />
+    <div className='flex justify-center h-screen'>
+      <CharacterMsg
+        msg='앗! 지금 만들고 있는 중이에요!'
+        imageUrl='/FixCat.svg'
+      />
     </div>
   );
 }
 
+export default withAuth(MylibraryPage);
---
+++ contexts/WithAuth.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useAuth } from "./AuthContext";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export function withAuth<P extends object>(
+  WrappedComponent: React.ComponentType<P>
+) {
+  return function WithAuth(props: P) {
+    const { user, loading } = useAuth();
+    const router = useRouter();
+
+    useEffect(() => {
+      if (!loading && !user) {
+        router.push("/login");
+      }
+    }, [user, loading, router]);
+
+    if (loading) {
+      return <div>Loading...</div>; // 또는 스켈레톤 UI
+    }
+
+    if (!user) {
+      return null; // 리다이렉트 중이므로 아무것도 렌더링하지 않음
+    }
+
+    return <WrappedComponent {...props} />;
+  };
+}
