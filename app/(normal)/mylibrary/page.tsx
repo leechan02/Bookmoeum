b0a6f86631c9b554bd2c203453c2eca1b5015b1e
Eui Lee <lechan1317@gmail.com>
2024-08-28 10:19:28 +0900

    style: mylibrary screen

--- app/(normal)/mylibrary/page.tsx
+++ app/(normal)/mylibrary/page.tsx
@@ -5,7 +5,7 @@ import { withAuth } from "@/contexts/WithAuth";
 
 function MylibraryPage() {
   return (
-    <div className='flex justify-center h-screen'>
+    <div className='flex justify-center items-center flex-grow'>
       <CharacterMsg
         msg='앗! 지금 만들고 있는 중이에요!'
         imageUrl='/FixCat.svg'
