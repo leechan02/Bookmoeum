793b060c772c5b49e2bd70aa6587f659ab48a4f0
Eui Lee <lechan1317@gmail.com>
2024-08-28 11:25:36 +0900

    refactor: LoginOption component

---
+++ app/(login)/_components/LoginOption.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import Button from "@/components/Button/Button";
+import Input from "@/components/Input/Input";
+import { auth } from "@/libs/firebase/config";
+import { signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth/web-extension";
+import { useRouter } from "next/navigation";
+
+interface LoginOptionProps {
+  onEmailCheck: (email: string) => void;
+}
+
+export default function LoginOption({ onEmailCheck }: LoginOptionProps) {
+  const router = useRouter();
+
+  const handleGoogleLogin = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      await signInWithPopup(auth, provider);
+
+      router.push("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleEmailSubmit = (email: string) => {
+    onEmailCheck(email);
+  };
+
+  return (
+    <div className='flex-col justify-center items-center gap-5 inline-flex'>
+      <div className='flex-col justify-start items-center gap-4 inline-flex'>
+        <Button
+          icon='/IconGoogle.svg'
+          label='구글로 시작하기'
+          variant='secondary'
+          width={400}
+          onClick={handleGoogleLogin}
+        />
+        <Button label='다른 옵션 없음' variant='secondary' width={400} />
+      </div>
+      <div className='inline-flex justify-center gap-1'>
+        <div className='border-b-2 border-secondary w-48 mb-[10px]'></div>
+        <div className='font-medium text-secondary text-base'>or</div>
+        <div className='border-b-2 border-secondary w-48 mb-[10px]'></div>
+      </div>
+      <Input
+        type='email'
+        placeholder='이메일을 입력하세요'
+        buttonLabel='계속'
+        onSubmit={handleEmailSubmit}
+      />
+    </div>
+  );
+}
--- app/(login)/_components/LoginSection.tsx
+++ app/(login)/_components/LoginSection.tsx
@@ -1,34 +1,13 @@
 "use client";
 
-import Button from "@/components/Button/Button";
-import Input from "@/components/Input/Input";
-import { auth } from "@/libs/firebase/config";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import LoginOption from "./LoginOption";
 
 interface LoginSectionProps {
   onEmailCheck: (email: string) => void;
 }
 
 export default function LoginSection({ onEmailCheck }: LoginSectionProps) {
-  const router = useRouter();
-
-  const handleGoogleLogin = async () => {
-    try {
-      const provider = new GoogleAuthProvider();
-      await signInWithPopup(auth, provider);
-
-      router.push("/");
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleEmailSubmit = (email: string) => {
-    onEmailCheck(email);
-  };
-
   return (
     <div className='inline-flex justify-center items-center w-1/2 bg-white'>
       <div className='flex-col justify-center items-center inline-flex'>
@@ -39,29 +18,7 @@ export default function LoginSection({ onEmailCheck }: LoginSectionProps) {
           <div className='text-center text-primary text-3xl font-medium'>
             반갑습니다!
           </div>
-          <div className='flex-col justify-center items-center gap-5 inline-flex'>
-            <div className='flex-col justify-start items-center gap-4 inline-flex'>
-              <Button
-                icon='/IconGoogle.svg'
-                label='구글로 시작하기'
-                variant='secondary'
-                width={400}
-                onClick={handleGoogleLogin}
-              />
-              <Button label='다른 옵션 없음' variant='secondary' width={400} />
-            </div>
-            <div className='inline-flex justify-center gap-1'>
-              <div className='border-b-2 border-secondary w-48 mb-[10px]'></div>
-              <div className='font-medium text-secondary text-base'>or</div>
-              <div className='border-b-2 border-secondary w-48 mb-[10px]'></div>
-            </div>
-            <Input
-              type='email'
-              placeholder='이메일을 입력하세요'
-              buttonLabel='계속'
-              onSubmit={handleEmailSubmit}
-            />
-          </div>
+          <LoginOption onEmailCheck={onEmailCheck} />
         </div>
       </div>
     </div>
