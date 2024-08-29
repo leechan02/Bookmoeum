f2a11cc37f8955a0d4841d3a70ef28518d0d0a78
Eui Lee <lechan1317@gmail.com>
2024-08-29 17:49:28 +0900

    feat: logo animation

--- app/(home)/_components/WhereSection.tsx
+++ app/(home)/_components/WhereSection.tsx
@@ -1,10 +1,56 @@
+"use client";
 import { FiBook, FiBox, FiHome } from "react-icons/fi";
 import WhereInfo from "./WhereInfo";
+import { motion, useAnimation, useInView } from "framer-motion";
+import { useRef, useEffect } from "react";
 
 export default function WhereSection() {
+  const sectionRef = useRef(null);
+  const controls = useAnimation();
+  const isInView = useInView(sectionRef, { once: false, amount: 0.3 });
+
+  useEffect(() => {
+    if (isInView) {
+      controls.start({
+        opacity: 1,
+        x: ['-40%', '0%', '40%', '0%', '-40%'],
+        y: ['-40%', '40%', '-40%', '40%', '-40%'],
+        transition: {
+          x: {
+            repeat: Infinity,
+            duration: 8,
+            ease: "easeInOut",
+          },
+          y: {
+            repeat: Infinity,
+            duration: 10,
+            ease: "easeInOut",
+          },
+          opacity: { duration: 0.5 }
+        }
+      });
+    } else {
+      controls.start({
+        opacity: 0,
+        transition: { duration: 0.5 }
+      });
+    }
+  }, [isInView, controls]);
+
   return (
-    <div className='min-h-screen flex justify-center items-center py-12'>
-      <div className='grid place-items-center gap-3 md:gap-6'>
+    <div ref={sectionRef} className='min-h-screen flex justify-center items-center py-12 relative overflow-hidden'>
+      <motion.img 
+        src="/IconMille.svg" 
+        alt="Background Icon" 
+        className="absolute w-16 h-16 rounded-full"
+        style={{
+          right: '20%',  // 오른쪽에서 10% 떨어진 위치
+          top: '25%',    // 위에서 25% 떨어진 위치
+        }}
+        initial={{ opacity: 0 }}
+        animate={controls}
+      />
+      <div className='grid place-items-center gap-3 md:gap-6 z-10'>
         <div className='text-primary text-xs md:text-sm font-medium'>
           책을 찾아볼 수 있는 곳
         </div>
