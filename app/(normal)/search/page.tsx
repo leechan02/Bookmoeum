3711435e8dea78064febf9ac2ea175ecdd4b5b6c
Eui Lee <lechan1317@gmail.com>
2024-08-30 12:14:53 +0900

    fix: suspense add in SearchPage

--- app/(normal)/search/page.tsx
+++ app/(normal)/search/page.tsx
@@ -2,7 +2,7 @@
 
 import { useSearchParams } from "next/navigation";
 import SearchTabs from "./_components/SearchTabs";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { Suspense, useCallback, useEffect, useRef, useState } from "react";
 import BookList from "@/components/Book/BookList";
 
 export interface SearchResult {
@@ -12,7 +12,7 @@ export interface SearchResult {
   isbn13: string;
 }
 
-export default function SearchPage(): JSX.Element {
+function SearchContent() {
   const searchParams = useSearchParams();
   const query = searchParams.get("query") || "검색어를 입력해주세요";
   const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
@@ -71,26 +71,37 @@ export default function SearchPage(): JSX.Element {
     fetchSearchResults();
   }, [query, page]);
 
+
+  return (
+    <div className='flex flex-col justify-center items-start gap-6 sm:gap-8'>
+      <div className='font-bold text-2xl sm:text-3xl text-primary'>
+        {query}
+      </div>
+      <SearchTabs />
+      {isLoading ? (
+        <div className='text-sm sm:text-base'>검색결과</div>
+      ) : (
+        <div className='text-sm sm:text-base'> 검색결과 {totalResults}</div>
+      )}
+      <BookList
+        searchResults={searchResults}
+        lastResultElementRef={lastResultElementRef}
+      />
+      {isLoading && <div className='text-sm sm:text-base'>로딩 중...</div>}
+      {!hasMore && (
+        <div className='text-sm sm:text-base'>더 이상 결과가 없습니다.</div>
+      )}
+    </div>
+  );
+}
+
+export default function SearchPage(): JSX.Element {
   return (
     <div className='w-full max-w-[1440px] mx-auto'>
-      <div className='flex flex-col justify-center items-start px-4 sm:px-6 md:px-8 lg:px-28 py-6 sm:py-8 gap-6 sm:gap-8'>
-        <div className='font-bold text-2xl sm:text-3xl text-primary'>
-          {query}
-        </div>
-        <SearchTabs />
-        {isLoading ? (
-          <div className='text-sm sm:text-base'>검색결과</div>
-        ) : (
-          <div className='text-sm sm:text-base'> 검색결과 {totalResults}</div>
-        )}
-        <BookList
-          searchResults={searchResults}
-          lastResultElementRef={lastResultElementRef}
-        />
-        {isLoading && <div className='text-sm sm:text-base'>로딩 중...</div>}
-        {!hasMore && (
-          <div className='text-sm sm:text-base'>더 이상 결과가 없습니다.</div>
-        )}
+      <div className='px-4 sm:px-6 md:px-8 lg:px-28 py-6 sm:py-8'>
+        <Suspense fallback={<div>Loading...</div>}>
+          <SearchContent />
+        </Suspense>
       </div>
     </div>
   );
