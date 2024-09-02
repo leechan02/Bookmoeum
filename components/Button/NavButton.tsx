e71605a787571bd2c9f3d2c2854b5ae6f30f5a33
Eui Lee <lechan1317@gmail.com>
2024-09-02 17:31:05 +0900

    feat: logout func on mobile nav

--- components/Button/NavButton.tsx
+++ components/Button/NavButton.tsx
@@ -4,21 +4,31 @@ import React from "react";
 interface NavButtonProps {
   icon: React.ElementType | string;
   label: string;
-  link: string;
+  link?: string;
+  onClick?: () => void;
 }
 
-export default function NavButton({ icon: Icon, label, link }: NavButtonProps) {
+function NavContent({ icon: Icon, label }: NavButtonProps) {
   return (
-    <Link
-      href={link}
-      className='flex flex-col gap-1 justify-center items-center'
-    >
+    <div className='flex flex-col gap-1 justify-center items-center w-12'>
       {typeof Icon === "string" ? (
         <img src={Icon} alt='icon' className='w-6 h-6' />
       ) : (
         <Icon className='w-6 h-6 text-secondary' />
       )}
       <div className='text-secondary font-regular text-xs'>{label}</div>
+    </div>
+  );
+}
+
+export default function NavButton({ icon, label, link, onClick }: NavButtonProps) {
+  return link ? (
+    <Link href={link}>
+      <NavContent icon={icon} label={label} />
     </Link>
+  ) : (
+    <button onClick={onClick}>
+      <NavContent icon={icon} label={label} />
+    </button>
   );
 }
--- components/Navigation/MobileNav.tsx
+++ components/Navigation/MobileNav.tsx
@@ -1,9 +1,12 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import NavButton from "../Button/NavButton";
-import { FiBook, FiUser } from "react-icons/fi";
+import { FiBook, FiLogOut, FiUser } from "react-icons/fi";
+import { useAuth } from "@/contexts/AuthContext";
+import { auth } from "@/libs/firebase/config";
 
 export default function MobileNav() {
+  const { user } = useAuth();
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
@@ -15,13 +18,21 @@ export default function MobileNav() {
     return () => window.removeEventListener("resize", checkMobile);
   }, []);
 
+  const handleLogout = () => {
+    auth.signOut();
+  };
+
   if (!isMobile) return null;
 
   return (
     <div className='flex fixed bottom-0 bg-primary w-full justify-center items-center gap-16 py-2 rounded-t-2xl'>
       <NavButton icon='/LogoIconDark.svg' label='홈' link='/' />
       <NavButton icon={FiBook} label='내 서재' link='/mylibrary' />
-      <NavButton icon={FiUser} label='로그인' link='/login' />
+      {user ? (
+        <NavButton icon={FiLogOut} label='로그아웃' onClick={handleLogout} />
+      ) : (
+        <NavButton icon={FiUser} label='로그인' link='/login' />
+      )}
     </div>
   );
 }
