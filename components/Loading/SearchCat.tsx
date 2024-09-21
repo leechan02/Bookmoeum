d40a25d3cb3ae116197a888393ce6e20e983066c
Eui Lee <lechan1317@gmail.com>
2024-09-21 10:45:29 +0900

    style: cat charactor position

--- components/Loading/SearchCat.tsx
+++ components/Loading/SearchCat.tsx
@@ -1,6 +1,6 @@
 export default function SearchCat() {
   return (
-    <div className='flex flex-col justify-center items-center w-full h-full'>
+    <div className='flex flex-col justify-center items-center w-full'>
       <video autoPlay loop muted playsInline width={300} height={300}>
         <source src='/video/SearchCat.mp4' type='video/mp4' />
         Your browser does not support the video tag.
