3ef7ea4516b0a098775a7e1de254cbda974d88c9
Eui Lee <lechan1317@gmail.com>
2024-10-03 11:08:04 +0900

    feat: icon adD

--- components/Input/SearchBar.tsx
+++ components/Input/SearchBar.tsx
@@ -2,6 +2,7 @@
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import { FiSearch } from "react-icons/fi";
+import { LuScanLine } from "react-icons/lu";
 
 interface SearchBarProps {
   placeholder?: string;
@@ -37,7 +38,7 @@ export default function SearchBar({
   const formClasses = `
     w-full
     ${small ? 'max-w-[300px] h-[40px]' : 'max-w-[460px] lg:max-w-[584px] h-[52px] md:h-[60px]'}
-    flex items-center px-6 py-2 rounded-full bg-secondary opacity-95 mx-2
+    flex gap-2 items-center px-6 py-2 rounded-full bg-secondary opacity-95 mx-2
   `;
 
   const inputClasses = `
@@ -63,6 +64,7 @@ export default function SearchBar({
       <button type='submit'>
         <FiSearch className={iconClasses} />
       </button>
+      <LuScanLine className={iconClasses} />
     </form>
   );
 }
